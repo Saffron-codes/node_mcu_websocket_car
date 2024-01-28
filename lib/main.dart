00e8da1215d03771a84b23398be2951e0b682d16
saffron-codes <saffrontdionysius@gmail.com>
2024-01-28 15:40:04 +0530

    update websocket IP

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,18 @@ import 'package:web_socket_channel/io.dart';
 import 'websocket_service/web_socket_service.dart';
 
 void main() async {
-  final channel =  IOWebSocketChannel.connect(
-    	// Node MCU Car IP
-	//Uri.parse('ws://192.168.4.1:81'),
-	
-	//Uri.parse('ws://localhost:87')
+  final url = Uri.parse("10.42.0.1:8765");
+  final channel = IOWebSocketChannel.connect(
+    url,
   );
+
+  // Node MCU Car IP
+  //Uri.parse('ws://192.168.4.1:81'),
+
+  //Uri.parse('ws://localhost:87')
+
   final webSocketService = WebSocketService(channel);
-  
+
   runApp(
     CarApp(
       webSocketService: webSocketService,
